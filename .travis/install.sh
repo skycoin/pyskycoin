34fb8475e6106d8383abc5a900b36f0df55120d5
Norge Fajardo Vega <fajardovega@gmail.com>
2018-06-23 16:47:12 -0400

    [ci] Fixes #16 Extend Travis-ci matrix to add OSX

---
+++ .travis/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -v
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update;
+    brew install swig;
+
+    case "${TOXENV}" in
+        py27)
+            # Install some custom Python 3.2 requeriments
+            brew install python;
+            pip install tox-travis;
+            ;;
+        py35)
+            # Install some custom Python 3.2 requeriments
+            brew install python3;
+            pip3 install tox-travis;
+            ;;
+    esac
+
+else
+    # Install some custom requeriments por Linux
+    pip install tox-travis;
+    eval "$(gimme 1.10)"
+    sudo apt-get update && sudo apt-get install swig
+fi
