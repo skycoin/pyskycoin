d116d63353bad3e717bf825b1db43a5adcaf9303
Norge Fajardo Vega <fajardovega@gmail.com>
2018-06-25 18:02:50 -0400

    [ci] refs #16 - Fix pip problems.

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,13 +5,16 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install openssl readline;
     brew outdated pyenv || brew upgrade pyenv;
     brew install pyenv-virtualenv
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile;
+    exec $SHELL;
     pyenv install $PYTHON
     export PYENV_VERSION=$PYTHON
     export PATH="/Users/travis/.pyenv/shims:${PATH}"
     brew install swig;
-    pyenv-virtualenv venv;
-    source venv/bin/activate;
+    pyenv virtualenv $PYTHON venv;
+    pyenv activate venv;
     python --version;
+    pip install tox;
 
     case "${TOXENV}" in
         py27)
