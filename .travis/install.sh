8184727c0045b3e50b26b88260234318b88f8088
Norge Fajardo Vega <fajardovega@gmail.com>
2018-06-26 10:56:16 -0400

    [ci] refs #16 - Force build.

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-set -e
-set -v
+set -ev
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew update;
+    brew outdated pyenv || brew upgrade pyenv;
+    brew install pyenv-virtualenv
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile;
+    exec $SHELL;
     brew install swig;
 
     case "${TOXENV}" in
         py27)
-            # Install some custom Python 3.2 requeriments
-            brew install python;
-            pip install tox-travis;
+            pyenv install $PYTHON
+            export PYENV_VERSION=$PYTHON
+            export PATH="/Users/travis/.pyenv/shims:${PATH}"
+            pyenv virtualenv $PYTHON venv;
+            pyenv activate venv;
+            python --version;
+            pip install tox;
+            # pip install tox-travis;
             ;;
         py35)
-            # Install some custom Python 3.2 requeriments
-            brew install python3;
             pip3 install tox-travis;
             ;;
     esac
