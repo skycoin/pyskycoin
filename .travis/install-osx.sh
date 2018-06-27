27b64333fbccf665b26d87e1c6d023a7940fee4a
Norge Fajardo Vega <fajardovega@gmail.com>
2018-06-27 14:30:40 -0400

    [ci] refs #16 - Refactor travis.yml file and create new one for OXS systems (.travis/install-osx.sh)

---
+++ .travis/install-osx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ev
+
+# Install some dependencies
+brew update;
+brew outdated pyenv || brew upgrade pyenv;
+brew install pyenv-virtualenv
+brew install swig;
+brew install gimme;
+brew install tox;
+
+# Install Python
+pyenv install $PYTHON
+
+# Prepare and initialize pyenv environment
+export PYENV_VERSION=$PYTHON
+export PATH="/Users/travis/.pyenv/shims:${PATH}"
+eval "$(pyenv init -)";
+eval "$(pyenv virtualenv-init -)";
+
+# Create and activate python virtual environment
+pyenv virtualenv $PYTHON venv;
+pyenv activate venv;
