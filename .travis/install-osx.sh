6b71fd83ae71f9f61c7371273fe531c49d161850
Olemis Lang <olemis+work@gmail.com>
2018-07-08 19:28:06 +0000

    [ci] refs #18 - Python 2.7 not found by pyenv global command
    Build regression in https://travis-ci.org/simelo/pyskycoin/builds/401506560

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -11,7 +11,7 @@ brew install gimme;
 
 # Install Python
 pyenv install $PYTHON
-pyenv global $PYTHON 2.7.14
+pyenv global $PYTHON 2.7
 
 # Prepare and initialize pyenv environment
 export PYENV_VERSION=$PYTHON
