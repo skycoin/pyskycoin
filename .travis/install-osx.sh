f59330d804285196c30f9ff368ad8742d811db2f
Olemis Lang <olemis+work@gmail.com>
2018-07-09 22:31:29 +0000

    [ci] refs #18 - Export path to pyenv interpreter in a separate env var

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -20,7 +20,8 @@ eval 'alias python$(echo $PYTHON | cut -d . -f 1,2)=$(pyenv which python3)'
 eval 'alias python2.7=$(pyenv which python2.7)'
 
 # Prepare and initialize pyenv environment
-PYENVPATH="$(dirname "$(pyenv which python${PYTHON})")" export PATH="$(dirname "$PYENVPATH"):/Users/travis/.pyenv/shims:${PATH}"
+export PYENVPATH="$(dirname "$(pyenv which python${PYTHON})")"
+export PATH="$PYENVPATH:/Users/travis/.pyenv/shims:${PATH}"
 echo "PATH=$PATH"
 eval "$(pyenv init -)";
 eval "$(pyenv virtualenv-init -)";
