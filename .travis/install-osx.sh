7537a746a04e1bb98d93ee529bfedcba8164e39c
Olemis Lang <olemis+work@gmail.com>
2018-07-10 04:02:27 +0000

    [ci] refs #18 - Create symlink for target Python interpreter

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -10,21 +10,23 @@ brew install swig;
 brew install gimme;
 
 # Install Python
-pyenv install $PYTHON
+pyenv install ${PYTHON}
 pyenv install 2.7.14
-pyenv global $PYTHON 2.7.14
+pyenv global ${PYTHON} 2.7.14
 pyenv versions
-pyenv which python${PYTHOB}
+pyenv which python${PYTHON}
 
 # Temp vars
-export PYCMDVERSION="$(echo $PYTHON | cut -d . -f 1,2)"
-export PYENVBINPATH="$(pyenv which python${PYCMDVERSION})"
+export PYCMD_VERSION="$(echo ${PYTHON} | cut -d . -f 1,2)"
+export PYCMD_PATH="$(pyenv which python${PYCMD_VERSION})"
+export PYCMD_DIRPATH="$( dirname ${PYCMD_PATH} )"
 
-export PATH="$( dirname ${PYENVBINPATH} ):/Users/travis/.pyenv/shims:${PATH}"
+export PATH="${PYCMD_DIRPATH}:/Users/travis/.pyenv/shims:${PATH}"
 echo "PATH=$PATH"
+sudo ln -s "${PYCMD_PATH}" "${PYCMD_DIRPATH}/python${PYCMD_VERSION}"
 
 # Define command aliases
-eval "alias python$PYCMDVERSION=$(pyenv which python$PYCMDVERSION)"
+eval "alias python${PYCMD_VERSION}=$(pyenv which python${PYCMD_VERSION})"
 eval "alias python2.7=$(pyenv which python2.7)"
 
 # Prepare and initialize pyenv environment
