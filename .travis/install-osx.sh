a03ec0d4b22557070f99c234e83af1442fd0b477
Olemis Lang <olemis+work@gmail.com>
2018-12-03 22:54:08 -0500

    [ci] refs #76 - Command alias in main Travis script (rather than install-osx.sh)

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -26,6 +26,3 @@ export PYCMD_PATH="$(pyenv which python${PYCMD_VERSION})"
 export PYCMD_DIRPATH="$( dirname ${PYCMD_PATH} )"
 export PATH="${PYCMD_DIRPATH}:/Users/travis/.pyenv/shims:${PATH}"
 
-# Define command aliases
-eval "alias python${PYCMD_VERSION}=$(pyenv which python${PYCMD_VERSION})" && eval "alias python2.7=$(pyenv which python2.7)"
-
