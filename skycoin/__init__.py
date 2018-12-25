d79e625e0af0db788ac4a75c23600480f038fc7f
Olemis Lang <olemis+work@gmail.com>
2018-12-25 15:28:10 -0500

    [ci] refs #67 - Ensure __version__ is always available in skycoin package. Silence package import failure

--- skycoin/__init__.py
+++ skycoin/__init__.py
@@ -1,3 +1,16 @@
-from .skycoin import *
-from .skyerror import *
-__version__ = "0.24.4"
+
+__version__ = "0.25.0"
+init_error = None
+
+try:
+    from .skycoin import *
+    from .skyerror import *
+except (AttributeError, ImportError) as _err :
+    init_error = _err
+
+    import sys, traceback
+    print("Error initializing skycoin package. Details:", file=sys.stderr)
+    exc_info = sys.exc_info()
+    traceback.print_exception(*exc_info)
+    print("\n\nInport succeeded but package load failed", file=sys.stderr)
+
