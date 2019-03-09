d47bf1af1402d22c369a4281f3563adc1ed0b6ce
Maykel Arias Torres <dev0006@simelo.tech>
2019-03-09 09:55:18 -0500

    [travis] refs #102 Added in .travis/install_docker docker-machine create default --driver xhyve --xhyve-experimental-nfs-share

--- .travis/install_docker.sh
+++ .travis/install_docker.sh
@@ -10,6 +10,7 @@ if [[ "$TRAVIS_OS_NAME" == 'osx' ]]; then
 brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve 
 sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
 eval $(docker-machine env default)
 docker-machine start default
 fi
