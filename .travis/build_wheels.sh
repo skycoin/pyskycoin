75c3c5915d593f2436dfb116e2122081428a7ce3
Maykel Arias Torres <dev0006@simelo.tech>
2019-05-05 16:26:44 -0400

    [travis] refs #107 Reapir error generate to https://travis-ci.org/skycoin/pyskycoin/builds/528546027#L3367

--- .travis/build_wheels.sh
+++ .travis/build_wheels.sh
@@ -2,13 +2,13 @@
 set -e -x
 
 # Install system packages required by our library
-yum install -y sudo pcre pcre-devel wget
+yum install -y sudo pcre pcre-devel
 mkdir -p "$HOME/bin"
 PIP=/opt/python/cp27-cp27m/bin/pip source /io/.travis/install-linux.sh
 eval "$(gimme 1.10)"
 
 # Install golang
-wget https://storage.googleapis.com/golang/go1.11.3.linux-amd64.tar.gz
+curl -sL -o "go1.11.3.linux-amd64.tar.gz" https://storage.googleapis.com/golang/go1.11.3.linux-amd64.tar.gz
 sudo tar -zxvf go1.11.3.linux-amd64.tar.gz -C /usr/local
 echo 'export GOROOT=/usr/local/go' | sudo tee -a /etc/profile
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile
