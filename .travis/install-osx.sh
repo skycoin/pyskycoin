be81708b6a658b9ffa69fe236df730d0ec591c58
Maykel Arias Torres <dev0006@simelo.tech>
2019-05-26 00:09:32 -0400

    [travis] refs #110 Declare pure export swig

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -7,9 +7,9 @@ brew update;
 brew outdated pyenv || brew upgrade pyenv;
 brew install pyenv-virtualenv
 brew install swig@3.04 || brew link --force swig@3.04;
-echo 'export PATH="/usr/local/opt/swig@3.04/bin:$PATH"' >> ~/.bash_profile;
+export PATH="/usr/local/opt/swig@3.04/bin:$PATH";
 ls -oa /usr/local/opt/swig@3.04/bin;
-source ~/.bash_profile;
+echo $PATH;
 brew install gimme;
 
 # Install Python
