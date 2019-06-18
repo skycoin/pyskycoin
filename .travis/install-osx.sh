7de2b0ff25b3a12a51efeae707d041c8130ec762
Maykel Arias Torres <dev0006@simelo.tech>
2019-06-18 08:53:01 -0400

    [travis] refs #140 Enable install pyenv

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -4,8 +4,8 @@ set -ev
 
 # Install some dependencies
 brew update;
-# brew outdated pyenv || brew upgrade pyenv;
-# brew install pyenv-virtualenv
+brew outdated pyenv || brew upgrade pyenv;
+brew install pyenv-virtualenv
 echo 'Creating swig@3.0.12 formula';
 cd "$(brew --repository)/Library/Taps/homebrew/homebrew-core";
 git show 42d31bba7772fb01f9ba442d9ee98b33a6e7a055:Formula/swig.rb | grep -v 'fails_with' > Formula/swig.rb;
@@ -14,20 +14,20 @@ brew install swig || brew link --overwrite swig;
 # brew install gimme;
 
 # Install Python
-# pyenv install ${PYTHON}
-# pyenv install 2.7.14
-# pyenv global ${PYTHON} 2.7.14
+pyenv install ${PYTHON}
+pyenv install 2.7.14
+pyenv global ${PYTHON} 2.7.14
 
 # Prepare and initialize pyenv environment
-# eval "$(pyenv init -)";
-# eval "$(pyenv virtualenv-init -)";
-# pyenv rehash
+eval "$(pyenv init -)";
+eval "$(pyenv virtualenv-init -)";
+pyenv rehash
 
 # Setup environment and PATH in MacOS
-# export PYCMD_VERSION="$(echo ${PYTHON} | cut -d . -f 1,2)"
-# export PYCMD_PATH="$(pyenv which python${PYCMD_VERSION})"
-# export PYCMD_DIRPATH="$( dirname ${PYCMD_PATH} )"
-# export PATH="${PYCMD_DIRPATH}:/Users/travis/.pyenv/shims:${PATH}"
+export PYCMD_VERSION="$(echo ${PYTHON} | cut -d . -f 1,2)"
+export PYCMD_PATH="$(pyenv which python${PYCMD_VERSION})"
+export PYCMD_DIRPATH="$( dirname ${PYCMD_PATH} )"
+export PATH="${PYCMD_DIRPATH}:/Users/travis/.pyenv/shims:${PATH}"
 
-python -m pip install --upgrade pip setuptools wheel tox tox-pyenv pytest pytest-runner
+eval "python${PYCMD_VERSION} -m pip install --upgrade pip setuptools wheel tox tox-pyenv pytest pytest-runner"
 
