9e2d230bb797679601dc590c1948dd5ebb360d7f
Maykel Arias Torres <dev0006@simelo.tech>
2019-06-18 11:03:15 -0400

    [travis] refs #140 Tryning install that brew

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -14,19 +14,39 @@ brew install swig || brew link --overwrite swig;
 # brew install gimme;
 
 # Install Python
-pyenv install ${PYTHON}
-pyenv global ${PYTHON}
+# pyenv install ${PYTHON}
+# pyenv global ${PYTHON}
 
 # Prepare and initialize pyenv environment
-eval "$(pyenv init -)";
-eval "$(pyenv virtualenv-init -)";
-pyenv rehash
+# eval "$(pyenv init -)";
+# eval "$(pyenv virtualenv-init -)";
+# pyenv rehash
 
 # Setup environment and PATH in MacOS
-export PYCMD_VERSION="$(echo ${PYTHON} | cut -d . -f 1,2)"
-export PYCMD_PATH="$(pyenv which python${PYCMD_VERSION})"
-export PYCMD_DIRPATH="$( dirname ${PYCMD_PATH} )"
-export PATH="${PYCMD_DIRPATH}:/Users/travis/.pyenv/shims:${PATH}"
+# export PYCMD_VERSION="$(echo ${PYTHON} | cut -d . -f 1,2)"
+# export PYCMD_PATH="$(pyenv which python${PYCMD_VERSION})"
+# export PYCMD_DIRPATH="$( dirname ${PYCMD_PATH} )"
+# export PATH="${PYCMD_DIRPATH}:/Users/travis/.pyenv/shims:${PATH}"
 
-eval "python${PYCMD_VERSION} -m pip install --upgrade pip setuptools wheel tox tox-pyenv pytest pytest-runner"
+# eval "python${PYCMD_VERSION} -m pip install --upgrade pip setuptools wheel tox tox-pyenv pytest pytest-runner"
+
+# Install by BREW
+
+if [[ "$TOXENV" == 'py34' ]]; then
+    brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/bd43f59bd50bb49242259f327cb6ac7a8dd59478/Formula/python3.rb
+    brew link --overwrite python
+fi
+
+if [[ "$TOXENV" == 'py35' ]]; then
+    brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/ec545d45d4512ace3570782283df4ecda6bb0044/Formula/python3.rb
+    brew link --overwrite python
+fi
+
+if [[ "$TOXENV" == 'py36' ]]; then
+    brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+    brew link --overwrite python
+fi
+
+
+python -m pip install --upgrade pip setuptools wheel tox tox-pyenv pytest pytest-runner
 
