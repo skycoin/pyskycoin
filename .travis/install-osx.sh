07d1778090bc2b386564f63f5e776321502d515d
Maykel Arias Torres <dev0006@simelo.tech>
2019-06-18 13:13:20 -0400

    [travis] refs #140 Tryning BREW in wget

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -30,30 +30,32 @@ brew install swig || brew link --overwrite swig;
 
 # eval "python${PYCMD_VERSION} -m pip install --upgrade pip setuptools wheel tox tox-pyenv pytest pytest-runner"
 
-# Install by BREW
+# Install by Homebrew
 
 echo 'Creating python formula';
-cd "$(brew --repository)/Library/Taps/homebrew/homebrew-core";
 echo 'Installing python';
 
 if [[ "$TOXENV" == 'py34' ]]; then
-    git show bd43f59bd50bb49242259f327cb6ac7a8dd59478:Formula/python3.rb | grep -v 'fails_with' > Formula/python.rb;
-    brew install python || brew link --overwrite python;
+    wget https://raw.githubusercontent.com/Homebrew/homebrew-core/bd43f59bd50bb49242259f327cb6ac7a8dd59478/Formula/python3.rb
+    cat python3.rb | grep -v 'fails_with' > Formula/python.rb
+    brew install python || brew link --overwrite python
 fi
 
 if [[ "$TOXENV" == 'py35' ]]; then
+    wget https://raw.githubusercontent.com/Homebrew/homebrew-core/ec545d45d4512ace3570782283df4ecda6bb0044/Formula/python3.rb
     git show ec545d45d4512ace3570782283df4ecda6bb0044:Formula/python3.rb | grep -v 'fails_with' > Formula/python.rb;
-    brew install python || brew link --overwrite python;
+    cat python3.rb | grep -v 'fails_with' > Formula/python.rb
+    brew install python || brew link --overwrite python
 fi
 
 if [[ "$TOXENV" == 'py36' ]]; then
-    git show f2a764ef944b1080be64bd88dca9a1d80130c558:Formula/python3.rb | grep -v 'fails_with' > Formula/python.rb;
-    brew install python || brew link --overwrite python;
+    wget https://raw.githubusercontent.com/Homebrew/homebrew-core/f2a764ef944b1080be64bd88dca9a1d80130c558/Formula/python.rb
+    cat python.rb | grep -v 'fails_with' > Formula/python.rb
+    brew install python || brew link --overwrite python
 fi
 
 if [[ "$TOXENV" == 'py37' ]]; then
-    git show 42d31bba7772fb01f9ba442d9ee98b33a6e7a055:Formula/python3.rb | grep -v 'fails_with' > Formula/python.rb;
-    brew install python || brew link --overwrite python;
+    brew install python || brew link --overwrite python
 fi
 
 python -m pip install --upgrade pip setuptools wheel tox tox-pyenv pytest pytest-runner
