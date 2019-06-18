61b9d89d449cee6d974708695f8c9003a0678b7c
Maykel Arias Torres <dev0006@simelo.tech>
2019-06-18 08:12:11 -0400

    [travis] refs #140 Commit all options in pyenv

--- .travis/install-osx.sh
+++ .travis/install-osx.sh
@@ -4,26 +4,26 @@ set -ev
 
 # Install some dependencies
 brew update;
-brew outdated pyenv || brew upgrade pyenv;
-brew install pyenv-virtualenv
+# brew outdated pyenv || brew upgrade pyenv;
+# brew install pyenv-virtualenv
 brew install swig;
 brew install gimme;
 
 # Install Python
-pyenv install ${PYTHON}
-pyenv install 2.7.14
-pyenv global ${PYTHON} 2.7.14
+# pyenv install ${PYTHON}
+# pyenv install 2.7.14
+# pyenv global ${PYTHON} 2.7.14
 
 # Prepare and initialize pyenv environment
-eval "$(pyenv init -)";
-eval "$(pyenv virtualenv-init -)";
-pyenv rehash
+# eval "$(pyenv init -)";
+# eval "$(pyenv virtualenv-init -)";
+# pyenv rehash
 
 # Setup environment and PATH in MacOS
-export PYCMD_VERSION="$(echo ${PYTHON} | cut -d . -f 1,2)"
-export PYCMD_PATH="$(pyenv which python${PYCMD_VERSION})"
-export PYCMD_DIRPATH="$( dirname ${PYCMD_PATH} )"
-export PATH="${PYCMD_DIRPATH}:/Users/travis/.pyenv/shims:${PATH}"
+# export PYCMD_VERSION="$(echo ${PYTHON} | cut -d . -f 1,2)"
+# export PYCMD_PATH="$(pyenv which python${PYCMD_VERSION})"
+# export PYCMD_DIRPATH="$( dirname ${PYCMD_PATH} )"
+# export PATH="${PYCMD_DIRPATH}:/Users/travis/.pyenv/shims:${PATH}"
 
-eval "python${PYCMD_VERSION} -m pip install --upgrade pip setuptools wheel tox tox-pyenv pytest pytest-runner"
+# eval "python${PYCMD_VERSION} -m pip install --upgrade pip setuptools wheel tox tox-pyenv pytest pytest-runner"
 
