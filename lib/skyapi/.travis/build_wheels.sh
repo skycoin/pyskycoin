0be14530d535db34f03bf5674964dc8736d273e6
Raul <riglesiasvera@gmail.com>
2019-07-01 16:40:16 -0400

    [skyapi] refs #131 trying to fix tavis's errors

---
+++ lib/skyapi/.travis/build_wheels.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -x
+
+# Install system packages required by our library
+yum install -y sudo pcre pcre-devel
+mkdir -p "$HOME/bin"
+PIP=/opt/python/cp27-cp27m/bin/pip source /io/.travis/install-linux.sh
+eval "$(gimme 1.10)"
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+  "${PYBIN}/pip" install -r /io/requirements.dev.txt
+  "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+  auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
