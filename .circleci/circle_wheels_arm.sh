10c0ab2dd61bcff7db6a00350ad5cd6d865e3aba
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-03 21:56:53 -0400

    [circle] refs #102 Remove `auditwheel`

--- .circleci/circle_wheels_arm.sh
+++ .circleci/circle_wheels_arm.sh
@@ -5,11 +5,11 @@
   # python3.6 -m pip wheel /io/ -w wheelhouse/
   # python3.7 -m pip wheel /io/ -w wheelhouse/
 
-# Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
-  python -m auditwheel repair "$whl" -w /io/wheelhouse/
-  # python3.4 -m auditwheel repair "$whl" -w /io/wheelhouse/
-  python3 -m auditwheel repair "$whl" -w /io/wheelhouse/
-  # python3.6 -m auditwheel repair "$whl" -w /io/wheelhouse/
-  # python3.7 -m auditwheel repair "$whl" -w /io/wheelhouse/
-done
+# # Bundle external shared libraries into the wheels
+# for whl in wheelhouse/*.whl; do
+#   python -m auditwheel repair "$whl" -w /io/wheelhouse/
+#   # python3.4 -m auditwheel repair "$whl" -w /io/wheelhouse/
+#   python3 -m auditwheel repair "$whl" -w /io/wheelhouse/
+#   # python3.6 -m auditwheel repair "$whl" -w /io/wheelhouse/
+#   # python3.7 -m auditwheel repair "$whl" -w /io/wheelhouse/
+# done
