a92543338b16f78c9965e78ce73075912ec22e0f
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-03 20:11:08 -0400

    [circle] refs #102 Only python 2.7 and python 3.5

--- .circleci/circle_wheels_arm.sh
+++ .circleci/circle_wheels_arm.sh
@@ -1,15 +1,15 @@
 # Compile wheels
   pip wheel /io/ -w wheelhouse/
-  python3.4 -m pip wheel /io/ -w wheelhouse/
+  # python3.4 -m pip wheel /io/ -w wheelhouse/
   pip3 wheel /io/ -w wheelhouse/
-  python3.6 -m pip wheel /io/ -w wheelhouse/
-  python3.7 -m pip wheel /io/ -w wheelhouse/
+  # python3.6 -m pip wheel /io/ -w wheelhouse/
+  # python3.7 -m pip wheel /io/ -w wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
   python -m auditwheel repair "$whl" -w /io/wheelhouse/
-  python3.4 -m auditwheel repair "$whl" -w /io/wheelhouse/
+  # python3.4 -m auditwheel repair "$whl" -w /io/wheelhouse/
   python3 -m auditwheel repair "$whl" -w /io/wheelhouse/
-  python3.6 -m auditwheel repair "$whl" -w /io/wheelhouse/
-  python3.7 -m auditwheel repair "$whl" -w /io/wheelhouse/
+  # python3.6 -m auditwheel repair "$whl" -w /io/wheelhouse/
+  # python3.7 -m auditwheel repair "$whl" -w /io/wheelhouse/
 done
