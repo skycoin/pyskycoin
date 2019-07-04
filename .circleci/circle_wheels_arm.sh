e5ca0cd6307fa24e0499f6679e66948f0b645f84
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-04 01:28:09 -0400

    [circle] refs #102 Correction path to wheelhouse

--- .circleci/circle_wheels_arm.sh
+++ .circleci/circle_wheels_arm.sh
@@ -1,7 +1,7 @@
 # Compile wheels
-  pip wheel /io/ -w wheelhouse/
+  pip wheel /io/ -w /io/wheelhouse/
   # python3.4 -m pip wheel /io/ -w wheelhouse/
-  pip3 wheel /io/ -w wheelhouse/
+  pip3 wheel /io/ -w /io/wheelhouse/
   # python3.6 -m pip wheel /io/ -w wheelhouse/
   # python3.7 -m pip wheel /io/ -w wheelhouse/
 
