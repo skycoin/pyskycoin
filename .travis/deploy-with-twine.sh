dec22462f479285500bd23dff882c1231a84b94f
Raul <riglesiasvera@gmail.com>
2019-07-07 17:19:10 -0400

    [skyapi] refs #131 changing permission of deploy-with-twine.sh

--- .travis/deploy-with-twine.sh
+++ .travis/deploy-with-twine.sh
@@ -1,11 +1,14 @@
 #Install twine
-pip install -U twine
+echo "Installing twine"
+python -m pip install twine
 
-#Make dist
+#Make wheels
+echo "Making wheels"
 make sdist
 make bdist_wheel
 if [ $TRAVIS_OS_NAME == "linux" && $TOXENV == "py37" ]; then make bdist_manylinux_amd64 ; fi
 
 #Upload to PyPi
+echo "Upload to PyPi"
 twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ dist/*
 twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ lib/skyapi/dist/*
