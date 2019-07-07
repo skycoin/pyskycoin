c7ef010209cf7413da6af6ae5ddf6b9d112984c3
Raul <riglesiasvera@gmail.com>
2019-07-07 16:56:50 -0400

    [skyapi] adding pypi user and password

--- .travis/deploy-with-twine.sh
+++ .travis/deploy-with-twine.sh
@@ -7,5 +7,5 @@ make bdist_wheel
 if [ $TRAVIS_OS_NAME == "linux" && $TOXENV == "py37" ]; then make bdist_manylinux_amd64 ; fi
 
 #Upload to PyPi
-twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
-twine upload --repository-url https://upload.pypi.org/legacy/ lib/skyapi/dist/*
+twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ dist/*
+twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ lib/skyapi/dist/*
