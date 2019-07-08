8d0a618087834fd2fb665c88827f0ac8f2394177
Raul <riglesiasvera@gmail.com>
2019-07-08 14:30:36 -0400

    [skyapi] refs #131 deploy to testPyPi

--- .travis/deploy-with-twine.sh
+++ .travis/deploy-with-twine.sh
@@ -8,7 +8,10 @@ make sdist
 make bdist_wheel
 if [ $TRAVIS_OS_NAME == "linux" && $TOXENV == "py37" ]; then make bdist_manylinux_amd64 ; fi
 
+echo "Upload to testPyPi"
+twine upload -u pyskycoin -p "prerelease_0.X" --verbose --repository-url https://test.pypi.org/legacy/ dist/*
+twine upload -u pyskycoin -p "prerelease_0.X" --verbose --repository-url https://test.pypi.org/legacy/ lib/skyapi/dist/*
 #Upload to PyPi
-echo "Upload to PyPi"
-twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ dist/*
-twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ lib/skyapi/dist/*
+# echo "Upload to PyPi"
+# twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ dist/*
+# twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ lib/skyapi/dist/*
