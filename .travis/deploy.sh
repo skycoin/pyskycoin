e0e74e6123829a4bc0bff68a248fcbb1858bd25a
Raul <riglesiasvera@gmail.com>
2019-07-08 17:24:25 -0400

    [skyapi] refs #131 upload only release-* branches to skyapi

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,9 +9,14 @@ make bdist_wheel
 if [ $TRAVIS_OS_NAME == "linux" && $TOXENV == "py37" ]; then make bdist_manylinux_amd64 ; fi
 
 # Upload
-echo "Upload to testPyPi"
+echo "Upload pyskycoin to testPyPi"
 twine upload -u pyskycoin -p "prerelease_0.X" --verbose --skip-existing --repository-url https://test.pypi.org/legacy/ dist/*
-twine upload -u pyskycoin -p "prerelease_0.X" --verbose --skip-existing --repository-url https://test.pypi.org/legacy/ lib/skyapi/dist/*
-# echo "Upload to PyPi"
-# twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ dist/*
-# twine upload -u $PYPITEST_USER -p $PYPITEST_PWD --repository-url https://upload.pypi.org/legacy/ lib/skyapi/dist/*
+
+branch="release-*"
+case $TRAVIS_BRANCH in
+	$branch)
+		echo "Upload skyapi to testPyPi"
+		twine upload -u pyskycoin -p "prerelease_0.X" --verbose --skip-existing --repository-url https://test.pypi.org/legacy/ lib/skyapi/dist/*;;
+	*)
+		echo "Not a release branch";;
+esac
