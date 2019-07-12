4da38f4038ad1ff8193a8a586d7615df36c40f3d
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-12 14:05:44 -0400

    [circle] refs #152 Repair path in checking auditwheel

--- .circleci/circle_wheels.sh
+++ .circleci/circle_wheels.sh
@@ -11,7 +11,9 @@ for PYBIN in /opt/python/*/bin; do
   "${PYBIN}/pip" wheel /io/lib/skyapi/ -w /io/wheelhouse/
 done
 
+ls -oa /io/wheelhouse
+
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in /io/wheelhouse/*.whl; do
   auditwheel repair "$whl" -w /io/wheelhouse/
 done
