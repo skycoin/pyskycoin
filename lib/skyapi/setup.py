927d361cd85d62f9533e714e71fe10d48aaf022d
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-12 15:59:03 -0400

    [skyapi] refs #152 Include README.md

--- lib/skyapi/setup.py
+++ lib/skyapi/setup.py
@@ -14,13 +14,26 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "skyapi"
-VERSION = "0.25.1.dev"
+VERSION = "0.25.1.dev01"
 # To install the library, run the following
 #
 # python setup.py install
 #
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
+
+from codecs import open
+from os import path
+import os
+import subprocess
+import platform
+import sys
+
+script_dirname = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(script_dirname, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 
@@ -35,7 +48,5 @@ setup(
     tests_require=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    Skycoin is a next-generation cryptocurrency.  # noqa: E501
-    """
+    long_description=long_description
 )
