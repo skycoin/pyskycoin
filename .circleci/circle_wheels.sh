3c744d604dc82a6d3afb3ee55899d5ca0dc47d37
Maykel Arias Torres <dev0006@simelo.tech>
2019-07-12 11:59:33 -0400

    [circle] refs #152 Separate all compilations

--- .circleci/circle_wheels.sh
+++ .circleci/circle_wheels.sh
@@ -1,10 +1,14 @@
 # Compile wheels
+echo "Compile pyskycoin"
 for PYBIN in /opt/python/*/bin; do
   "${PYBIN}/pip" install -r /io/requirements.dev.txt
-  "${PYBIN}/pip" wheel /io/ -w wheelhouse/
-  "${PYBIN}/pip" install -r /io/lib/skyapi/requirements.txt
-  "${PYBIN}/pip" wheel /io/lib/skyapi/ -w wheelhouse/
+  "${PYBIN}/pip" wheel /io/ -w /io/wheelhouse/
 done
+
+echo "Compile skyapi"
+for PYBIN in /opt/python/*/bin; do
+  "${PYBIN}/pip" install -r /io/lib/skyapi/requirements.txt
+  "${PYBIN}/pip" wheel /io/lib/skyapi/ -w /io/wheelhouse/
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
