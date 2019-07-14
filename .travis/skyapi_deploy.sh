f58eb4dc10324a12916654a94dccbfaf116514f6
Raul <riglesiasvera@gmail.com>
2019-07-13 21:09:03 -0400

    [skyapi] refs #131 skyapi upload 0.25.1.dev04

--- .travis/skyapi_deploy.sh
+++ .travis/skyapi_deploy.sh
@@ -2,6 +2,11 @@
 echo "Installing twine"
 python -m pip install twine
 
-#Upload to PyPi
-echo "Upload to PyPI"
-twine upload -u $PYPI_USER -p $PYPI_PASSWD --repository-url https://upload.pypi.org/legacy/ lib/skyapi/dist/*
+echo "Generating distribution archives before deploy"
+make sdist
+make bdist_wheel
+if [ $TRAVIS_OS_NAME == "linux" ]; then make bdist_manylinux_amd64 ; fi
+
+#Upload to testPyPI
+echo "Upload to testPyPI"
+twine upload -u pyskycoin -p "prerelease_0.X" --skip-existing --repository-url https://test.pypi.org/legacy/ lib/skyapi/dist/*
