86e3db7fed30d72ff1de991caa36a022b9f62838
Maykel Arias Torres <dev0006@simelo.tech>
2019-09-17 23:11:36 -0400

    [libpy] refs #169 Finish write testing `SKY_Handle_Strings_GetCount`

--- tests/test_util_param_distribution.py
+++ tests/test_util_param_distribution.py
@@ -5,18 +5,26 @@ import tests.utils as utils
 def test_TestDistributionAddressArrays():
     err, dist = skycoin.SKY_params_Distribution_GetMainNetDistribution()
     assert err == skycoin.SKY_OK
-    err, handle_addr = skycoin.SKY_params_Distribution_GetAddresses(dist)
+    err, addr = skycoin.SKY_params_Distribution_GetAddresses(dist)
     assert err == skycoin.SKY_OK
-    err, length_addr = skycoin.SKY_Handle_Strings_GetCount(handle_addr)
+    err, length_addr = skycoin.SKY_Handle_Strings_GetCount(addr)
     assert err == skycoin.SKY_OK
 
     assert length_addr == 100
     #  At the time of this writing, there should be 25 addresses in the
     #  unlocked pool and 75 in the locked pool.
-    # unlocked = skycoin.SKY_params_GetUnlockedDistributionAddresses()
-    # assert len(unlocked) == 25
-    # locked = skycoin.SKY_params_GetLockedDistributionAddresses()
-    # assert len(locked) == 75
+    err, unlocked = skycoin.SKY_params_Distribution_UnlockedAddresses(dist)
+    assert err == skycoin.SKY_OK
+    err, countUnlocked = skycoin.SKY_Handle_Strings_GetCount(unlocked)
+    assert err == skycoin.SKY_OK
+    assert countUnlocked == 25
+    err, locked = skycoin.SKY_params_Distribution_LockedAddresses(dist)
+    assert err == skycoin.SKY_OK
+    err, countLocked = skycoin.SKY_Handle_Strings_GetCount(locked)
+    assert err == skycoin.SKY_OK
+    assert countLocked == 75
+    # err, string = skycoin.SKY_Handle_Strings_GetAt(unlocked, 1)
+    # assert err == skycoin.SKY_OK
     # for i in range(len(addrs)):
     #     iStr = addrs[i]
     #     for j in range(i + 1):
