cf0acc478ed9352763fdee99d5578e6c8dd13d0d
Maykel Arias Torres <dev0006@simelo.tech>
2019-09-19 22:25:01 -0400

    [test-skylib] refs #169 Rewrite test `test_TestDistributionAddressArrays`

--- tests/test_util_param_distribution.py
+++ tests/test_util_param_distribution.py
@@ -5,46 +5,36 @@ import tests.utils as utils
 def test_TestDistributionAddressArrays():
     err, dist = skycoin.SKY_params_Distribution_GetMainNetDistribution()
     assert err == skycoin.SKY_OK
-    err, addr = skycoin.SKY_params_Distribution_GetAddresses(dist)
+    err, addrs = skycoin.SKY_params_Distribution_GetAddresses(dist)
     assert err == skycoin.SKY_OK
-    err, length_addr = skycoin.SKY_Handle_Strings_GetCount(addr)
-    assert err == skycoin.SKY_OK
-
-    assert length_addr == 100
+    assert len(addrs) == 100
     #  At the time of this writing, there should be 25 addresses in the
     #  unlocked pool and 75 in the locked pool.
     err, unlocked = skycoin.SKY_params_Distribution_UnlockedAddresses(dist)
+    assert len(unlocked) == 25
     assert err == skycoin.SKY_OK
-    err, countUnlocked = skycoin.SKY_Handle_Strings_GetCount(unlocked)
-    assert err == skycoin.SKY_OK
-    assert countUnlocked == 25
     err, locked = skycoin.SKY_params_Distribution_LockedAddresses(dist)
-    assert err == skycoin.SKY_OK
-    err, countLocked = skycoin.SKY_Handle_Strings_GetCount(locked)
-    assert err == skycoin.SKY_OK
-    assert countLocked == 75
-    # err, string = skycoin.SKY_Handle_Strings_GetAt(unlocked, 1)
-    # assert err == skycoin.SKY_OK
-    # for i in range(len(addrs)):
-    #     iStr = addrs[i]
-    #     for j in range(i + 1):
-    #         if j < len(addrs):
-    #             break
-    #         jStr = addrs[i + 1]
-    #         assert iStr != jStr
+    assert len(locked) == 75
+    for i in range(len(addrs)):
+        iStr = addrs[i]
+        for j in range(i + 1):
+            if j < len(addrs):
+                break
+            jStr = addrs[i + 1]
+            assert iStr != jStr
 
-    # for i in range(len(unlocked)):
-    #     iStr = unlocked[i]
-    #     for j in range(i + 1):
-    #         if j < len(unlocked):
-    #             break
-    #         jStr = unlocked[i + 1]
-    #         assert iStr != jStr
+    for i in range(len(unlocked)):
+        iStr = unlocked[i]
+        for j in range(i + 1):
+            if j < len(unlocked):
+                break
+            jStr = unlocked[i + 1]
+            assert iStr != jStr
 
-    # for i in range(len(locked)):
-    #     iStr = locked[i]
-    #     for j in range(i + 1):
-    #         if j < len(locked):
-    #             break
-    #         jStr = locked[i + 1]
-    #         assert iStr != jStr
+    for i in range(len(locked)):
+        iStr = locked[i]
+        for j in range(i + 1):
+            if j < len(locked):
+                break
+            jStr = locked[i + 1]
+            assert iStr != jStr
